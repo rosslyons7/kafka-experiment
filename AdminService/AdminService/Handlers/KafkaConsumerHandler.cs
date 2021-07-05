7c745b86603d2a2d229777bb004d0e804b6c0b18
Ross Lyons <rosslyons7@gmail.com>
2021-07-05 12:32:04 +0100

    AdminService consumes/produces

---
+++ AdminService/AdminService/Handlers/KafkaConsumerHandler.cs
@@ -0,0 +1,46 @@
+﻿using Confluent.Kafka;
+using Microsoft.Extensions.Hosting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using Kafka.Public;
+using Microsoft.Extensions.Logging;
+using Kafka.Public.Loggers;
+using System.Text;
+
+namespace AdminService.Handlers {
+    public class KafkaConsumerHandler : IHostedService {
+
+        private readonly string topic = "AdminTopic";
+        private readonly ILogger<KafkaConsumerHandler> _logger;
+        private ClusterClient _cluster;
+
+        public KafkaConsumerHandler(ILogger<KafkaConsumerHandler> logger) {
+            _logger = logger;
+            _cluster = new ClusterClient(new Configuration()
+            {
+                Seeds = "localhost:9092"
+            }, new ConsoleLogger());
+        }
+
+        public Task StartAsync(CancellationToken cancellationToken) {
+
+            _cluster.ConsumeFromEarliest(topic);
+            _cluster.MessageReceived += record =>
+            {
+                Console.WriteLine($"Received: {Encoding.UTF8.GetString(record.Value as byte[])}");
+                _logger.LogInformation($"Received: {Encoding.UTF8.GetString(record.Value as byte[])}");
+            };
+            return Task.CompletedTask;
+
+        }
+
+        public Task StopAsync(CancellationToken cancellationToken) {
+            _cluster?.Dispose();
+            return Task.CompletedTask;
+        }
+
+    }
+}
--- AdminService/AdminService/Startup.cs
+++ AdminService/AdminService/Startup.cs
@@ -1,3 +1,4 @@
+using AdminService.Handlers;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -22,6 +23,7 @@ namespace AdminService {
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services) {
             services.AddControllers();
+            services.AddSingleton<IHostedService, KafkaConsumerHandler>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
